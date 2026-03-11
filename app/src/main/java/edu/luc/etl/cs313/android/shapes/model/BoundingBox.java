fa2904c88dd24f1beeb4e05ef083ef86dcc00a94
gkallmeyer <gkallmeyer@luc.edu>
2026-03-11 15:41:59 -0500

    BoundingBox Completed

--- app/src/main/java/edu/luc/etl/cs313/android/shapes/model/BoundingBox.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/model/BoundingBox.java
@@ -17,38 +17,65 @@ public class BoundingBox implements Visitor<Location> {
 
     @Override
     public Location onFill(final Fill f) {
-        return null;
+        return f.getShape().accept(this);
     }
 
     @Override
     public Location onGroup(final Group g) {
-
-        return null;
+        int minX = Integer.MAX_VALUE;
+        int minY = Integer.MAX_VALUE;
+        int maxX = Integer.MIN_VALUE;
+        int maxY = Integer.MIN_VALUE;
+        for (Shape s : g.getShapes()) {
+            final Location box = s.accept(this);
+            final int x = box.getX();
+            final int y = box.getY();
+            final Rectangle r = (Rectangle) box.getShape();
+            minX = Math.min(minX, x);
+            minY = Math.min(minY, y);
+            maxX = Math.max(maxX, x + r.getWidth());
+            maxY = Math.max(maxY, y + r.getHeight());
+        }
+        return new Location(minX, minY, new Rectangle(maxX - minX, maxY - minY));
     }
 
     @Override
     public Location onLocation(final Location l) {
-
-        return null;
+        final Location bbox = l.getShape().accept(this);
+        return new Location(
+                l.getX() + bbox.getX(),
+                l.getY() + bbox.getY(),
+                bbox.getShape()
+        );
     }
 
     @Override
     public Location onRectangle(final Rectangle r) {
-        return null;
+        return new Location (0, 0, new Rectangle(r.getWidth(), r.getHeight()));
     }
 
     @Override
     public Location onStrokeColor(final StrokeColor c) {
-        return null;
+        return c.getShape().accept(this);
     }
 
     @Override
     public Location onOutline(final Outline o) {
-        return null;
+        return o.getShape().accept(this);
     }
 
     @Override
     public Location onPolygon(final Polygon s) {
-        return null;
+        int minX = Integer.MAX_VALUE;
+        int minY = Integer.MAX_VALUE;
+        int maxX = Integer.MIN_VALUE;
+        int maxY = Integer.MIN_VALUE;
+        for (Point p : s.getPoints()) {
+            minX = Math.min(minX, p.getX());
+            minY = Math.min(minY, p.getY());
+            maxX = Math.max(maxX, p.getX());
+            maxY = Math.max(maxY, p.getY());
+        }
+        return new Location(minX, minY, new Rectangle(maxX - minX, maxY - minY));
     }
 }
